3aaf3c29669d613cc5bf5e190c5d413aca8e9cde
unknown <kdodeja62@gmail.com>
2024-01-28 01:30:27 +0530

    Clean Application Structure and Created Login - Register Authentication Structure

--- client/src/Auth/Login/Login.js
+++ client/src/Auth/Login/Login.js
@@ -1,9 +1,29 @@
-import React from 'react'
-
 export const Login = () => {
   return (
-    <div>
-      Login
+    <div className="login-component">
+      <div>
+        <h1 className="login-header">Login to Download</h1>
+      </div>
+      <div>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          className="login-email"
+          required
+        />{" "}
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          className="login-password"
+          required
+        />{" "}
+        <button>Login</button>
+        <button>Sign Up</button>
+      </div>
     </div>
-  )
-}
+  );
+};
--- client/src/Auth/Register/Register.js
+++ client/src/Auth/Register/Register.js
@@ -1,9 +1,53 @@
-import React from 'react'
-
 export const Register = () => {
   return (
-    <div>
-      Register
+    <div className="register-component">
+      <div>
+        <h1 className="register-header">New User</h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          id="companyName"
+          name="companyName"
+          placeholder="Enter Company Name"
+          className="register-companyName"
+          required
+        />
+        <input
+          type="text"
+          id="location"
+          name="location"
+          placeholder="Enter Location"
+          className="register-location"
+          required
+        />
+        <input
+          type="text"
+          id="phoneNumber"
+          name="phoneNumber"
+          placeholder="Enter Phone Number"
+          className="register-phoneNumber"
+          required
+        />
+        <input
+          type="email"
+          id="emailId"
+          name="emailId"
+          placeholder="Enter Email ID"
+          className="register-email"
+          required
+        />
+        <textarea
+          id="remarks"
+          name="remarks"
+          rows="4"
+          cols="50"
+          placeholder="Enter Remarks"
+          className="register-remarks"
+          required
+        ></textarea>
+        <button>Submit</button>
+      </div>
     </div>
-  )
-}
+  );
+};
