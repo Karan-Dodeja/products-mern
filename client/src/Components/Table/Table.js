fcba98f1f3748c9820640125eaa0a06b7c956cb9
unknown <kdodeja62@gmail.com>
2024-01-28 00:11:23 +0530

    Change Table Design

--- client/src/Components/Table/Table.js
+++ client/src/Components/Table/Table.js
@@ -1,122 +1,164 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
-const tableHeader = () => {
-  return (
-    <div className="table-header">
-      <div>
-        <h1>Select Product(s)</h1>
-      </div>
-      <div>
-        <button>Download PDF</button>
-      </div>
-    </div>
-  );
-};
 
 export const Table = () => {
   return (
     <div className="table-component">
       <div className="table-data">
         <table>
-          <th>{tableHeader}</th>
+          <tr>
+            <th>
+              <div className="table-header">
+                <div>
+                  <h1>Select Product(s)</h1>
+                </div>
+                <div>
+                  <button>Download PDF</button>
+                </div>
+              </div>
+            </th>
+          </tr>
+
           <tr>
             <td>
-              <input
-                type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
-              />
-            </td>
-            <td>
-              <label>Chemistry 1</label>
-            </td>
-            <td>
-              <label>Process 1.2</label>
-            </td>
-            <td>
-              <Link to="/">View Details</Link>
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                  />
+                </div>
+                <div>
+                  <label>Chemistry 1</label>
+                </div>
+                <div>
+                  <label>Process 1.1</label>
+                </div>
+                <div>
+                  <Link to="/">View Details</Link>
+                </div>
+              </div>
             </td>
           </tr>
 
           <tr>
             <td>
-              <input
-                type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
-              />
-            </td>
-            <td>
-              <label>Chemistry 2</label>
-            </td>
-            <td>
-              <label>Process 2.1</label>
-            </td>
-            <td>
-              <Link to="/">View Details</Link>
-            </td>
-          </tr>
-
-          <tr>
-            <td>
-              <input
-                type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
-              />
-            </td>
-            <td>
-              <label>Chemistry 2</label>
-            </td>
-            <td>
-              <label>Process 2.2</label>
-            </td>
-            <td>
-              <Link to="/">View Details</Link>
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                  />
+                </div>
+                <div>
+                  <label>Chemistry 1</label>
+                </div>
+                <div>
+                  <label>Process 1.2</label>
+                </div>
+                <div>
+                  <Link to="/">View Details</Link>
+                </div>
+              </div>
             </td>
           </tr>
 
           <tr>
             <td>
-              <input
-                type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
-              />
-            </td>
-            <td>
-              <label>Chemistry 3</label>
-            </td>
-            <td>
-              <label>Process 3.1</label>
-            </td>
-            <td>
-              <Link to="/">View Details</Link>
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                  />
+                </div>
+                <div>
+                  <label>Chemistry 2</label>
+                </div>
+                <div>
+                  <label>Process 2.1</label>
+                </div>
+                <div>
+                  <Link to="/">View Details</Link>
+                </div>
+              </div>
             </td>
           </tr>
 
           <tr>
             <td>
-              <input
-                type="checkbox"
-                id="vehicle1"
-                name="vehicle1"
-                value="Bike"
-              />
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                  />
+                </div>
+                <div>
+                  <label>Chemistry 2</label>
+                </div>
+                <div>
+                  <label>Process 2.2</label>
+                </div>
+                <div>
+                  <Link to="/">View Details</Link>
+                </div>
+              </div>
             </td>
+          </tr>
+
+          <tr>
             <td>
-              <label>Chemistry 3</label>
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                  />
+                </div>
+                <div>
+                  <label>Chemistry 3</label>
+                </div>
+                <div>
+                  <label>Process 3.1</label>
+                </div>
+                <div>
+                  <Link to="/">View Details</Link>
+                </div>
+              </div>
             </td>
+          </tr>
+
+          <tr>
             <td>
-              <label>Process 3.2</label>
-            </td>
-            <td>
-              <Link to="/">View Details</Link>
+              <div>
+                <div>
+                  <input
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                  />
+                </div>
+                <div>
+                  <label>Chemistry 3</label>
+                </div>
+                <div>
+                  <label>Process 3.2</label>
+                </div>
+                <div>
+                  <Link to="/">View Details</Link>
+                </div>
+              </div>
             </td>
           </tr>
         </table>
